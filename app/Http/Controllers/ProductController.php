2fb1482458ac8b16e14dff3806b2adb73cb66eb1
Karen Bonganay <kbonganay.intern.integratr@outlook.com>
2023-03-30 17:59:57 +0800

    initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products= Product::all();
+
+        return response()->json($products,200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $payload = $this->payload($request);
+
+        $product = Product::create($payload);
+
+        return response()->json($product,200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::where ('id',$id)->first();
+        return response()->json($product,200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $payload = $this->payload($request);
+
+        $product = Product::where ('id',$id)->first();
+
+        $product->update($payload);
+
+        return response()->json($product,200);
+
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::where ('id',$id)->first();
+        $product->delete();
+        return response('',200);
+    }
+
+    public function payload($request){
+        return $this->validate($request,[
+            'product_name' => ['required'],
+            'quantity'=>['required']
+        ]);
+    }
+}
